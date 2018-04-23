79bad38b717ee25bf3f505584dc1c4e1daa3641e
Nan Xiao <me@nanx.me>
2018-04-23 09:35:07 +0000

    version 1.8

--- inst/doc/grex.R
+++ inst/doc/grex.R
@@ -3,13 +3,14 @@ options(width = 90)
 
 ## ---------------------------------------------------------------------------------------
 library("grex")
-data("gtexv6p")
-id = gtexv6p[101:200]
+data("gtexv7")
+id = gtexv7[101:200]
 df = grex(id)
 tail(df)
 
 ## ---------------------------------------------------------------------------------------
-filtered_genes = df[!is.na(df$"entrez_id"),
-                    c("ensembl_id", "entrez_id", "hgnc_symbol", "gene_biotype")]
+filtered_genes =
+  df[!is.na(df$"entrez_id"),
+     c("ensembl_id", "entrez_id", "hgnc_symbol", "gene_biotype")]
 head(filtered_genes)
 
